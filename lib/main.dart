d4abbf1607e5169dddb94d731c521966057286cd
WhiteWolf720 <diegooy.ti20@utsjr.edu.mx>
2023-05-24 21:20:32 -0600

    implementacion de login

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:contador/pages/home_page.dart';
+import 'package:contador/pages/login_page.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
@@ -8,9 +8,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Material App',
-      home: HomePage(),
+      home: LoginPage(),
     );
   }
 }
---
+++ lib/pages/login_page.dart
@@ -0,0 +1,255 @@
+import "package:flutter/material.dart";
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: const [
+          Fondo(),
+          Contenido(),
+        ],
+      ),
+    );
+  }
+}
+
+class Contenido extends StatefulWidget {
+  const Contenido({super.key});
+
+  @override
+  State<Contenido> createState() => _ContenidoState();
+}
+
+class _ContenidoState extends State<Contenido> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text('Login',
+            style: TextStyle(color: Colors.white,
+            fontSize: 30,
+            fontWeight: FontWeight.bold,
+            ),
+          ),
+          const SizedBox(height: 5,),
+          const Text('welcome to your acount',
+            style: TextStyle(color: Colors.white,
+            fontSize: 10,
+            letterSpacing: 1.5,
+            ),
+          ),
+          const SizedBox(height: 5,),
+          const Datos(),
+        ]
+        ),
+    );
+  }
+}
+
+class Datos extends StatefulWidget {
+  const Datos({super.key});
+
+  @override
+  State<Datos> createState() => _DatosState();
+}
+
+class _DatosState extends State<Datos> {
+  bool obs = true;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(20),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        color: Colors.white
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text('Email',
+          style: TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.bold,
+            fontSize: 20,
+            ),
+          ),
+          const SizedBox(height: 5,),
+          TextFormField(
+            keyboardType: TextInputType.emailAddress,
+            decoration: const InputDecoration(
+              border: OutlineInputBorder(),
+              hintText: 'email@example.com',
+            ),
+          ),
+          const SizedBox(height: 5,),
+          const Text('Password',
+           style: TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.bold,
+            fontSize: 20,
+            ),
+          ),
+          const SizedBox(height: 5,),
+          TextFormField(
+            obscureText: obs,
+            decoration: InputDecoration(
+              border: const OutlineInputBorder(),
+              hintText: 'password here, boy',
+              suffixIcon: IconButton(
+                icon: const Icon(
+                  Icons.remove_red_eye_outlined,
+                ),
+                onPressed: () {
+                  setState( () {
+                    obs == true ? obs = false : obs = true;
+                  });
+                },
+              ),
+
+
+            ),
+          ),
+          const Remember(),
+          const SizedBox(height: 5,),
+          const Botones(),
+        ],  
+      ),
+    );
+  }
+}
+
+class Remember extends StatefulWidget {
+  const Remember({super.key});
+
+  @override
+  State<Remember> createState() => _RememberState();
+}
+
+class _RememberState extends State<Remember> {
+  bool valor = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Checkbox(value: valor, 
+        onChanged: (value) {
+          setState(() {
+            valor == false? valor = true : valor = false;
+          });
+        } 
+        ),
+        const Text('Recuerdame ☝️🤓'),
+        const Spacer(),
+        TextButton(
+          onPressed: () {}, 
+          child: const Text('Forgot password?')
+          )
+      ],
+    );
+  }
+}
+
+class Botones extends StatelessWidget {
+  const Botones({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SizedBox(
+          width: double.infinity,
+          height: 50,
+          child: ElevatedButton(
+            onPressed:() {},
+            style: ButtonStyle(
+              backgroundColor: 
+              MaterialStateProperty.all<Color>(const Color(0xff142047))
+            ),
+            child: const Text(
+              'login',
+              style: TextStyle(
+                color: Colors.white
+              ),
+            ),
+          )
+        ),
+        const SizedBox(
+          width: double.infinity,
+          height: 25,
+        ),
+        const Text(
+          'Or sign with',
+          style: TextStyle(
+            color: Colors.grey),
+        ),
+        const SizedBox(
+          width: double.infinity,
+          height: 25,
+        ),
+        SizedBox(
+          width: double.infinity,
+          height: 50,
+          child: OutlinedButton(
+            onPressed: (){}, 
+            child: const Text(
+              'sign with Google',
+              style: TextStyle(
+                color: Color(0xff142047),
+                fontSize: 18
+              ),
+            )),
+        ), //google pa
+        const SizedBox(
+          width: double.infinity,
+          height: 25,
+        ),
+        SizedBox(
+          width: double.infinity,
+          height: 50,
+          child: OutlinedButton(
+            onPressed: (){}, 
+            child: const Text(
+              'sign with Facebook',
+              style: TextStyle(
+                color: Color(0xff142047),
+                fontSize: 18
+              ),
+            )),
+        ), //facebook pa
+      ],
+    );
+  }
+}
+
+class Fondo extends StatelessWidget {
+  const Fondo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Colors.blue.shade200,
+            Colors.blue
+          ],
+          begin: Alignment.centerRight,
+          end: Alignment.centerLeft
+        )
+      ) 
+    );
+  }
+}
